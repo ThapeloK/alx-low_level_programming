7e66e6ff30dff73bde3c8af97e4c61d77d35d984
ThapeloK <e.thapelo@yahoo.com>
2020-09-16 18:47:43 +0000

    Add puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#includes <stdio.h>
+/**
+*C program that prints 
+*Specified words
+*/
+
+int main(void)
+{
+    printf("Programming is like building a multilingual puzzle");
+
+    return 0;
+
+}
