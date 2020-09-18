33a2088db471b2d112c761799ad54c40cc755600
ThapeloK <e.thapelo@yahoo.com>
2020-09-18 15:55:33 +0000

    Add README file and Task postive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- *main - Positive or Negative checker entry point
+ * main - Positive or Negative chekcer entry point
  *
- *Return: 0 at the end (Sucess)
+ * Return: 0 at the end (Success)
  */
 int main(void)
 {
@@ -12,12 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else 
+	if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("0 is zero\n");
+	}
 	return (0);
 }
