6c044d30882555624ea640df847f2764fdf686ce
ThapeloK <e.thapelo@yahoo.com>
2020-09-18 17:49:08 +0000

    Add last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
-	else (lastDigit < 6)
+	else if (lastDigit < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
