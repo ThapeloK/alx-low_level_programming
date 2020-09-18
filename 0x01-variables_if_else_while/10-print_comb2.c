09547abb52f0a959dae4415981df83cd7c48d9ac
ThapeloK <e.thapelo@yahoo.com>
2020-09-18 19:12:50 +0000

    Add 00...99  task

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers between 00 and 99 entry point
+ * Return: 0 at the end (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 57 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
