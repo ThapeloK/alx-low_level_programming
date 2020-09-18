3dc6880a781cc1b9ae34846cd9f791a98ba02a25
ThapeloK <e.thapelo@yahoo.com>
2020-09-18 18:02:49 +0000

    Add alphabetical order task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabetical order entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
