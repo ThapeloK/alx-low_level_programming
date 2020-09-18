a637a749e66d467c47104882ab8072fba2c1bb9c
ThapeloK <e.thapelo@yahoo.com>
2020-09-18 20:00:23 +0000

    Add advanced  task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - prints all possible different combinations of two digits.
+ * main - prints all possible different combinations of two digits
  * 
  * Return: 0
  */
