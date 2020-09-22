21a8cffe9df4f2c86ef9aa034d8076be093cc740
ThapeloK <e.thapelo@yahoo.com>
2020-09-22 14:36:35 +0000

    Add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints lower alpabectical letter in order 
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
