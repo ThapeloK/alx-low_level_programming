14c6021b2714a81db16311b92bddf420c03e1ae3
ThapeloK <e.thapelo@yahoo.com>
2020-09-22 14:41:18 +0000

    Add task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,15 @@
  * main - Entry point
  *
  * Description: prints lower alpabectical letter in order 
- * Return: Always 0.
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
 }
