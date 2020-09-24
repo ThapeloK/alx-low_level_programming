65a37ca5e3d5b0358361b40a53b2001e4ccec07b
ThapeloK <e.thapelo@yahoo.com>
2020-09-24 17:33:38 +0000

    Fix task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,21 @@ int main(void)
 	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
-		else if (!(i % 3))
-			printf("Fizz ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("Buzz\n");
 	return (0);
