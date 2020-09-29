2926d486b2562a4298b30718205980b58027a168
ThapeloK <e.thapelo@yahoo.com>
2020-09-29 20:14:25 +0000

    Add task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of a and b
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;  
+	temp = *a, 
+		*a = *b, 
+		*b = temp;
+}
