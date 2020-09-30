e0fe3e33b95eb62f9cef49d63b27ad62467a0505
ThapeloK <e.thapelo@yahoo.com>
2020-09-30 19:48:17 +0000

    Add task 6 - 9

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string 
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != 0; c++)
+		if (c % 2 == 0)
+			_putchar(str[c]);
+	_putchar('\n');
+}
