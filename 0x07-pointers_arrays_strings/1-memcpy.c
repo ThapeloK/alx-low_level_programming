eaf001d5c2ea679d269346168fe309416f364f07
ThapeloK <e.thapelo@yahoo.com>
2020-10-06 11:53:18 +0400

    Add task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	    unsigned int i = 0;
 
-	while (i < n)
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	return (dest);
+	    while (i < n)
+	    {
+		    *(dest + i) = *(src + i);
+		    i++;
+	    }
+	    return (dest);
 }
