ec787b857a60c5df7466e59e21756cf9b925ed81
ThapeloK <e.thapelo@yahoo.com>
2020-10-14 14:46:03 +0000

    Add task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
+
 /**
- * str_concat - concatenates two strings
- * @s1: input string 1
- * @s2: input string 2
- * Return: pointer to to the concatenated string
+ * str_concat - concatenaste two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: pointer to the concatenated string
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *pstr;
-	unsigned int len1 = 0, len2 = 0, i, j;
+	int length1 = 0, length2 = 0;
+	char *ret;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	while (s1[len1] != 0)
-		len1++;
-	while (s2[len2] != 0)
-		len2++;
-	len2++;
-	pstr = malloc(sizeof(char) * (len1 + len2));
-	for (i = 0; i < len1; i++)
-		pstr[i] = s1[i];
-	for (j = 0; j < len2; i++, j++)
-		pstr[i] = s2[j];
-	return (pstr);
+	while (s1 && *s1++)
+		length1++;
+	while (s2 && *s2++)
+		length2++;
+	ret = malloc(sizeof(char) * (length1 + length2 + 1));
+	if (!ret)
+		return (NULL);
+	ret += length1 + length2;
+	*ret = '\0';
+	for (s2--; length2--;)
+		*--ret = *--s2;
+	for (s1--; length1--;)
+		*--ret = *--s1;
+	return (ret);
 }
