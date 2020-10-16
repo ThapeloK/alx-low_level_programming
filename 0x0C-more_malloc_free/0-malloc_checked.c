1636848f39084628465ae1509e62e5419e658776
ThapeloK <e.thapelo@yahoo.com>
2020-10-16 23:12:23 +0400

    Add readme file and task o

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,11 +9,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-        void *mem = malloc(b);
-        if (mem == NULL)
-                exit(98);
+    void *mem = malloc(b);
+    if (mem == NULL)
+            exit(98);
 
-        return (mem);
+    return (mem);
 
 
 }
