0cf124a9cd08697b65dff2df057a259fb959bbbc
ThapeloK <e.thapelo@yahoo.com>
2020-10-16 22:46:28 +0400

    Add readme file and task o

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
@@ -8,9 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	 void *mem = malloc(b);
+  void *mem = malloc(b);
 
-	  if (mem == NULL)
-		  exit(98);
+    if (mem == NULL)
+    exit(98);
+
+  return (mem);
+
 
 }
