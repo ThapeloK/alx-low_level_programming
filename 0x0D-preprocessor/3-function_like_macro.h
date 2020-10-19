981cb9d0a7da4fda4c32c89bf66c941fa86a7316
ThapeloK <e.thapelo@yahoo.com>
2020-10-19 18:45:27 +0000

    Fix task 3 typo

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef HOLBERTON_3
-#define HOLBERTON_3
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
