4325e6d6f2c9177b1ab966fbf6347235ce8e5a2f
ThapeloK <e.thapelo@yahoo.com>
2020-11-04 20:23:57 +0000

    Fix task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
-	return (i);
+
+	return (nodes);
 }
