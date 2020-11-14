fcb435a35887c3a8576aac580b98e2b07cd8b9cb
ThapeloK <e.thapelo@yahoo.com>
2020-11-14 06:34:50 +0000

    Fix task 1 typo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, i;
 
-	retval = file = i = 0;
+	file = i = 0;
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
