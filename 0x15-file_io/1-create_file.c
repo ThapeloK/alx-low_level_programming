2f4e8f8628d86ed172271032af4171835daca7d1
ThapeloK <e.thapelo@yahoo.com>
2020-11-14 06:25:51 +0000

    Added readme file and completed project files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * create_file - creates a file
@@ -12,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, i;
 
-	file = i = 0;
+	retval = file = i = 0;
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * append_text_to_file - appends text to a file
